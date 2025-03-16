e785e8cae21aa33bbdbb6d6100c85e11874da5ee
Kurt Gilpin <kurt@gilpin.info>
2025-03-16 17:39:04 -0500

    Cleaning up unused zones

--- abysmal/Talida_Vermoy.lua
+++
@@ -1,8 +0,0 @@
-function event_say(e)
-    e.self:Say("Grumby over there is in charge of the maintenance department.  It is best if you do not disturb him.  He is very busy.");
-end
-
-function event_trade(e)
-	local item_lib = require("items");
-	item_lib.return_items(e.self, e.other, e.trade);
-end
--- txevu/297039.lua
+++
@@ -1,6 +0,0 @@
---trash ph in arena
-function event_combat(e)
-  if e.joined then
-    eq.depop_with_timer();
-  end
-end
