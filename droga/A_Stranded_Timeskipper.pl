23a3b04a732a655d908f99388aa4e599d9030b99
Kurt Gilpin <kurt@gilpin.info>
2025-03-17 00:37:56 -0500

    Droga Timeskipper

--- droga/A_Stranded_Timeskipper.pl
+++ droga/A_Stranded_Timeskipper.pl
@@ -1,4 +1,12 @@
-#generic soulbinder quest
 sub EVENT_SAY {
-  plugin::timeskipper_say($text);
+  if ($text=~/hail/i) {
+	  quest::say("Greetings, ${name}! I have been sent back through time to prepare you. If you would like to see [" . quest::saylink("what the future holds") . "] for this zone, just say so.");
+  }
+  elsif ($text=~/what the future holds/i) {
+    if ($zoneid == 81){
+      quest::doanim(42);
+	    quest::say("Off you go!");
+	    quest::MovePCInstance(81, 2, 2965, 3275, 345, 0)
+	  }
+  }
 }
