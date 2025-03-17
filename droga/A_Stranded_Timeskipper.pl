b1b0c4b7a01c35aa9dd0a22b79be2dff265ef4c4
Kurt Gilpin <kurt@gilpin.info>
2025-03-17 01:00:09 -0500

    CT & Droga Timeskippers

--- droga/A_Stranded_Timeskipper.pl
+++ droga/A_Stranded_Timeskipper.pl
@@ -1,12 +1,12 @@
 sub EVENT_SAY {
   if ($text=~/hail/i) {
-	  quest::say("Greetings, ${name}! I have been sent back through time to prepare you. If you would like to see [" . quest::saylink("what the future holds") . "] for this zone, just say so.");
+    quest::say("Greetings, ${name}! I have been sent back through time to prepare you. If you would like to see [" . quest::saylink("what the future holds") . "] for this zone, just say so.");
   }
   elsif ($text=~/what the future holds/i) {
     if ($zoneid == 81){
       quest::doanim(42);
-	    quest::say("Off you go!");
-	    quest::MovePCInstance(81, 2, 2965, 3275, 345, 0)
-	  }
+      quest::say("Off you go!");
+      quest::MovePCInstance(81, 2, 376, 1412, 4, 300)
+    }
   }
 }
