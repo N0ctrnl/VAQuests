dadb4c05ec9c1a104c3cef66096946f4fec945fe
Kurt Gilpin <kurt@gilpin.info>
2025-03-28 17:27:00 -0500

    Improve random number detection scripting in Dannel

--- bazaar/Collector_Dannel.pl
+++ bazaar/Collector_Dannel.pl
@@ -11,25 +11,25 @@ sub EVENT_ITEM {
     
   # Common Reward - 0-4500
   ## Buff Crystals
-  if((plugin::check_handin(\%itemcount, 150003 => 1) || plugin::check_handin(\%itemcount, 150004 => 1) || plugin::check_handin(\%itemcount, 150005 => 1)) && $random_result<4500){
+  if((plugin::check_handin(\%itemcount, 150003 => 1) || plugin::check_handin(\%itemcount, 150004 => 1) || plugin::check_handin(\%itemcount, 150005 => 1)) && $random_result <= 4500){
     quest::summonitem(150000,50);
   }
 
   ## 20-slot Bags
-  elsif(plugin::check_handin(\%itemcount, 150021 => 1) && $random_result<4500){
+  elsif(plugin::check_handin(\%itemcount, 150021 => 1) && $random_result <= 4500){
     quest::summonitem(quest::ChooseRandom(77404,81803));
     plugin::Whisper("Thank you so much! He's what I call a normal bag. I think you'll be impressed!");
   }
   
   # Rare Reward - 3001-4700
   ## Illusions
-  elsif((plugin::check_handin(\%itemcount, 150003 => 1) || plugin::check_handin(\%itemcount, 150004 => 1) || plugin::check_handin(\%itemcount, 150005 => 1)) && ($random_result>4500 && $random_result<4951)){
+  elsif((plugin::check_handin(\%itemcount, 150003 => 1) || plugin::check_handin(\%itemcount, 150004 => 1) || plugin::check_handin(\%itemcount, 150005 => 1)) && ($random_result => 4501 && $random_result <= 4950)){
     quest::summonitem(quest::ChooseRandom(40612,40613,40684,40685,40714,40746,40778,40779,43971,43972,54833,54913,40638));
     plugin::Whisper("This token is in fine shape. Use this item to mask your appearance.");
   }
 
   ## 32-slot Bags
-  elsif((plugin::check_handin(\%itemcount, 150021 => 1)) && ($random_result>4500 && $random_result<4951)){
+  elsif((plugin::check_handin(\%itemcount, 150021 => 1)) && ($random_result => 4501 && $random_result <= 4950)){
     quest::summonitem(quest::ChooseRandom(77405,67633));
     plugin::Whisper("Thank you so much! He's what I call a medium bag. I think you'll be impressed!");
   }
@@ -40,13 +40,13 @@ sub EVENT_ITEM {
   #### Guise of the Deceiver
   #### Runed Fighter's Staff
   #### Holgresh Elder Beads
-  elsif((plugin::check_handin(\%itemcount, 150003 => 1) || plugin::check_handin(\%itemcount, 150004 => 1) || plugin::check_handin(\%itemcount, 150005 => 1)) && ($random_result>4950 && $random_result<5001)){
+  elsif((plugin::check_handin(\%itemcount, 150003 => 1) || plugin::check_handin(\%itemcount, 150004 => 1) || plugin::check_handin(\%itemcount, 150005 => 1)) && ($random_result => 4951 && $random_result <= 5000)){
     quest::summonitem(quest::ChooseRandom(14727,14730,2469,6630,24890));
     plugin::Whisper("This token is in fantastic condition! Here's something from my private stock!");
   }
 
   ## 40-slot Bags
-  elsif(plugin::check_handin(\%itemcount, 150021  => 1) && ($random_result>4950 && $random_result<5001)){
+  elsif(plugin::check_handin(\%itemcount, 150021  => 1) && ($random_result => 4951 && $random_result <= 5000)){
     quest::summonitem(quest::ChooseRandom(139831,101686));
     plugin::Whisper("Thank you so much! He's what I call a large bag. I think you'll be impressed!");
   }
