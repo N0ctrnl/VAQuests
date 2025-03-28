753735f9df489e57d328c02ef4e2590f13980d8b
Kurt Gilpin <kurt@gilpin.info>
2025-03-28 17:11:29 -0500

    Finalizing Dannel with bag quests working

--- bazaar/Collector_Dannel.pl
+++ bazaar/Collector_Dannel.pl
@@ -16,7 +16,7 @@ sub EVENT_ITEM {
   }
 
   ## 20-slot Bags
-  elsif(plugin::check_handin(\%itemcount, XXX => 1) && $random_result<4500){
+  elsif(plugin::check_handin(\%itemcount, 150021 => 1) && $random_result<4500){
     quest::summonitem(quest::ChooseRandom(77404,81803));
     plugin::Whisper("Thank you so much! He's what I call a normal bag. I think you'll be impressed!");
   }
