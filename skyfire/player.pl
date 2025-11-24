f4339df1ce14b5f20e76a3a2fc601f0e19bb8e5e
Kurt Gilpin <kurt@gilpin.info>
2025-11-23 23:51:09 -0600

    Add Vallon event controller with spawn and despawn logic. Add proper VP key to door script

--- skyfire/player.pl
+++ skyfire/player.pl
@@ -28,13 +28,17 @@ sub EVENT_CLICKDOOR {
     $client->Message(4,"The globe does not seem to do anything");
   }
   if($doorid == 135) {
+    # Old VP Key: 20884
+    if(plugin::check_hasitem($client, 20884) && !$client->KeyRingCheck(20884)) {
+      $client->KeyRingAdd(20884);
+    }
     if(plugin::check_hasitem($client, 69311) && !$client->KeyRingCheck(69311)) {
       $client->KeyRingAdd(69311);
     }
     if(plugin::check_hasitem($client, 69312) && !$client->KeyRingCheck(69312)) {
       $client->KeyRingAdd(69312);
     }
-    if($client->KeyRingCheck(69311) || $client->KeyRingCheck(69312) || ($status > 99)) {
+    if($client->KeyRingCheck(69311) || $client->KeyRingCheck(69312) || $client->KeyRingCheck(20884) || ($status > 99)) {
       quest::movepc(108,1682,41,25.9); # Zone: veeshan
     }
     else {
